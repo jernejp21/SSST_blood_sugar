0e665127c0f27ec4c41f15de866dd1ed483b409d
jernejp21 <jernej.pangerc@tutamail.com>
2022-03-15 11:38:06 +0900

    enh: Program architecture #1
    Implemented program architecture. Only functions, variables are not complete.

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -9,6 +9,8 @@
 #include <drivers/gpio.h>
 #include <zephyr.h>
 
+#include <bluetooth/conn.h>
+
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 1000
 
@@ -27,28 +29,121 @@
 #define FLAGS 0
 #endif
 
+/* Defines */
+#define RAW_DATA_SIZE 10  // This will change after testing
+#define AVG_DATA_SIZE 100  // This will change after testing
+
+/* Variables */
+uint8_t isInternalAdc = 1;
+uint16_t adcAveragedData[AVG_DATA_SIZE];
+uint16_t adcRawData[RAW_DATA_SIZE];
+
+struct bt_conn_cb btConnectionCallback;
+
+static void systemInit()
+{
+  // Peripheral initialization
+
+  // BT initialization
+}
+
+static void BTN_AdcSwitch()
+{
+  // Switch between internal and external ADC. Interrupt
+  if(isInternalAdc == 1)
+  {
+    // Switch to external ADC
+
+    isInternalAdc = 0;
+  }
+  else
+  {
+    // Switch to internal ADC
+
+    isInternalAdc = 1;
+  }
+}
+
+static void BTN_ShutDown()
+{
+  // Check if button is pressed for 3 s. Polling.
+}
+
+static uint16_t adcSampleAverage(uint16_t* array, size_t size)
+{
+  uint32_t avg;
+
+  for(size_t index = 0; index < size; index++)
+  {
+    avg = avg + *(array + index);
+  }
+
+  avg = avg / size;
+
+  return (uint16_t)avg;
+}
+
+static void readADC()
+{
+  if(isInternalAdc == 1)
+  {
+    // Read internal ADC
+  }
+  else
+  {
+    // Read external ADC
+  }
+}
+
+static void BT_Connected(struct bt_conn* conn, uint8_t err)
+{
+  // Start ADC, EasyDMA
+}
+
+static void BT_Disconnected(struct bt_conn* conn, uint8_t reason)
+{
+  // Stop ADC, EasyDMA
+}
+
+static void BT_SendData()
+{
+  // Send averaged and compressed data
+}
+
+static void compressData(uint16_t* rawData, uint16_t* compressedData)
+{
+}
+
+static void LED_PowerOn()
+{
+}
+
+static void LED_PowerOff()
+{
+}
+
+static void LED_externalAdcSelected()
+{
+}
+
+static void LED_internalAdcSelected()
+{
+}
+
+static void LED_LowBattery()
+{
+}
+
 void main(void)
 {
-  const struct device *dev;
-  bool led_is_on = true;
-  int ret;
-
-  dev = device_get_binding(LED0);
-  if(dev == NULL)
-  {
-    return;
-  }
-
-  ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-  if(ret < 0)
-  {
-    return;
-  }
+  // systemInit();
+  //  BT, ADC, LEDs, GPIOs everything is prepared
 
   while(1)
   {
-    gpio_pin_set(dev, PIN, (int)led_is_on);
-    led_is_on = !led_is_on;
-    k_msleep(SLEEP_TIME_MS);
+    // k_uptime_get_32();
+    BTN_ShutDown();  // read on 50 ms interval
+    readADC();  // read on sampling interval
+    BT_SendData();  // send when buffer full
   }
 }
