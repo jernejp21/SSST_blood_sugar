48774f733a1db637aa9bf37ce56d324f383a6faa
jernejp21 <jernej.pangerc@tutamail.com>
2022-04-07 10:24:27 +0900

    Changelog update

--- generate_chlog.sh
+++ generate_chlog.sh
@@ -1,1 +1,1 @@
-github_changelog_generator -u AZURTEST-CORP -p SSST_blood_sugar -t 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 --output Changelog.md
+github_changelog_generator -u AZURTEST-CORP -p SSST_blood_sugar -t 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 --output Changelog.md --no-pull-requests --no-filter-by-milestone
